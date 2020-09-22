0366011d43352e175cbf3aef7921ded53d121b5f
David <davidchall388@gmail.com>
2020-09-22 09:20:35 -0400

    carsouel bugs worked out, timing fine tuned, next/prev buttons working

---
+++ app.js
@@ -0,0 +1,14 @@
+$(() => {
+
+    $('.carousel').carousel({
+        interval: 3000
+    })
+
+//     next = () => {
+//         $('.carousel').carousel('next')
+//     }
+
+//     previous = () => {
+//         $('.carousel').carousel('prev')
+//     }
+} )
