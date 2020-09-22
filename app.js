b33571481b24658e3fd2c41dd02f62a309fe2b75
David <davidchall388@gmail.com>
2020-09-22 13:05:39 -0400

    tile hover images in place, more tiles added

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ $(() => {
         interval: 3000
     })
 
+
+
+    $('.second-button').on('click', () =>  {
+
+    $('.animated-icon2').toggleClass('open');
+    });
+
+
 //     next = () => {
 //         $('.carousel').carousel('next')
 //     }
